0f9ead13a6cd4a9b6d24533d381ce42e5e034c50
beingpandey87 <pranav.pandey0705@gmail.com>
2023-10-28 13:23:25 +0530

    updated ml models

--- ml_model.py
+++ ml_model.py
@@ -3,12 +3,12 @@
 print("adding all ML related code here")
 
 def ml_model1():
-    print("adding code for ml model1")
+    print("adding code for ml model 1")
 
 def ml_model2():
-    print("adding code for ml model2")
+    print("adding code for ml model 2")
 
 def ml_model3():
-    print("adding code for ml model3")
+    print("adding code for ml model 3")
 
-print("model 2 is performing best")
+print("model two is performing best")
