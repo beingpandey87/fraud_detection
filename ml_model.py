4be797ebeff5c9ac9eb3671860f29cc0815c85aa
beingpandey87 <pranav.pandey0705@gmail.com>
2023-10-28 13:08:35 +0530

    added three new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,12 @@
 
 
 print("adding all ML related code here")
+
+def ml_model1():
+    print("adding code for ml model1")
+
+def ml_model2():
+    print("adding code for ml model2")
+
+def ml_model3():
+    print("adding code for ml model3")
